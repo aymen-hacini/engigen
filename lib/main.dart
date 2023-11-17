6d89535999140e207b0ea94bf1220f564fc84b33
aymen <https://github.com/aymen-hacini>
2023-11-17 17:04:24 +0100

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:engigen/Auth/signup_page.dart';
-import 'package:engigen/home_page.dart';
-import 'package:engigen/link_hub.dart';
-import 'package:engigen/workers.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
@@ -22,6 +19,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Engigen App', home: SignupPage());
+    return MaterialApp(title: 'Engigen App', home: SignupPage());
   }
 }
